f959aef9650a57c73bda45d03e39b41ec9902823
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 17:21:07 +0300

    added comment form

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,7 +4,7 @@ from wtforms import validators
 from wtforms.validators import Required
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    bio = TextAreaField('Tell us about you:',validators = [Required()])
     submit = SubmitField('Submit')
 
 class PitchForm(FlaskForm):
@@ -12,3 +12,8 @@ class PitchForm(FlaskForm):
   category = SelectField("Choose your desired category:",choices=[('Experience','Experience'),('Technology','Technology'),('Education','Education'),('Humour','Humour'),('Intelligence','Intelligence')])
   pitch = TextAreaField('Add text here:',validators=[Required()])
   submit = SubmitField('Submit')
+
+
+class CommentForm(FlaskForm):
+  comment = TextAreaField('Add a Comment:',validators=[Required()])
+  submit = SubmitField('Submit')      
