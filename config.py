213418fc77cff4526f57b97ea829e7fc36a4b8a5
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 11:37:34 +0300

    created config module and added config classes

---
+++ config.py
@@ -0,0 +1,66 @@
+import os
+class Config:
+
+    '''
+    
+    General configuration parent class
+    
+    '''
+
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch_flask'
+    SECRET_KEY='nickson00841999code'
+
+   
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+
+
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+
+
+config_options = {
+
+    'development':DevConfig,
+    'production':ProdConfig,
+}    
+
+
+
+
+
+
