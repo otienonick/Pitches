3b7c272b6da5a0d0eee0ca2d6ac7b246e720142a
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 13:42:40 +0300

    added Newpitch form class and created instance variables required

--- app/main/forms.py
+++ app/main/forms.py
@@ -6,3 +6,9 @@ from wtforms.validators import Required
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
     submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+  title = StringField(' Title of your pitch:',validators=[Required()])
+  category = SelectField("Choose your desired category:",choices=[('Experience','Experience'),('Technology','Technology'),('Education','Education'),('Humour','Humour'),('Intelligence','Intelligence')])
+  pitch = TextAreaField('Add text here:',validators=[Required()])
+  submit = SubmitField('Submit')
