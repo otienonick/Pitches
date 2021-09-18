9e81807dbb800ccdd7d1e978c5de9cd030f18c19
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 23:50:57 +0300

    added tests folder and added tests

---
+++ tests/test_comment.py
@@ -0,0 +1,29 @@
+import unittest
+from app.models import Comment,User
+from app import db
+
+class TestReview(unittest.TestCase):
+
+    def setUp(self):
+        self.user_James = User(username = 'James',password = 'potato', email = 'james@ms.com')
+        self.new_comment = Comment(comment='Review for movies',title="life",author = self.user_James )
+
+    def tearDown(self):
+        Comment.query.delete()
+        User.query.delete()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_comment,Comment))
+
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_comment.comment, 'Review for movies')
+        self.assertEquals(self.new_comment.title,"life")
+        self.assertEquals(self.new_comment.author, self.user_James)
+
+    def test_save_review(self):
+        self.new_comment.save_comment()
+        self.assertTrue(len(Comment.query.all())>0)
+
+
---
+++ tests/test_pitch.py
@@ -0,0 +1,25 @@
+import unittest
+from app.models import User,Pitch
+from app import db
+
+class TestReview(unittest.TestCase):
+
+    def setUp(self):
+        self.user_James = User(username = 'James',password = 'potato', email = 'james@ms.com')
+        self.new_pitch  = Pitch( pitch = 'life is a mess',category = 'education',name = 'nick',author = self.user_James )
+
+    def tearDown(self):
+        User.query.delete()
+        Pitch.query.delete()
+
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitch))
+
+
+    def test_check_instance_variables(self):
+        self.assertEquals(self.new_pitch.pitch, 'life is a mess')
+        self.assertEquals(self.new_pitch.category,"education")
+        self.assertEquals(self.new_pitch.name,"nick")
+        self.assertEquals(self.new_pitch.author, self.user_James)
+
