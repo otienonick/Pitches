a5e2d7182f330698715c424bef9ac3568d509e14
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 17:23:19 +0300

    added comment class and db instances

--- app/models.py
+++ app/models.py
@@ -53,9 +53,6 @@ class Role(db.Model):
 
 class Pitch(db.Model):
 
-  '''
-  Class to define pitch objects
-  '''
   __tablename__ = 'pitches'
 
   id = db.Column(db.Integer,primary_key=True)
@@ -65,5 +62,22 @@ class Pitch(db.Model):
   author = db.Column(db.Integer,db.ForeignKey('users.id'))
 
   def __repr__(self):
-    return f'Pitch{self.pitch}'        
+    return f'Pitch{self.pitch}'       
+
+    
+class Comment(db.Model):
+
+ 
+  __tablename__ = 'comments'
+  id = db.Column(db.Integer,primary_key=True)
+  comment = db.Column(db.String(255))
+  title = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+  author = db.Column(db.Integer,db.ForeignKey('users.id'))
+  
+  def save_comment(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def __repr__(self):
+    return f'{self.comment}'     
 
