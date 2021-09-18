8078468ad5100e8b3c32c86aa350eb665e5e7e2e
otienonick <nick.odongi@student.moringaschool.com>
2021-09-18 22:00:01 +0300

    created upvotes and downvote functions

---
+++ app/static/js/scripts.js
@@ -0,0 +1,11 @@
+var counter = 0;
+function upvote(){
+    counter++;
+    document.getElementById('upvote').innerHTML = counter;
+}
+
+var counting  = 0;
+function downvote(){
+    counting++;
+    document.getElementById('downvote').innerHTML = counting;
+}
