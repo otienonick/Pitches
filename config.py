de9194313fc9156ba9ab0bcdf2fb9d24b48838c0
otienonick <nick.odongi@student.moringaschool.com>
2021-09-20 00:34:12 +0300

    deployment to heroku

--- config.py
+++ config.py
@@ -1,56 +1,58 @@
 import os
-
 class Config:
 
     '''
-    
     General configuration parent class
     
     '''
 
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY='nickson00841999code'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch_flask'
-
-
-
-
-     #email configurations
+   
+    
+    #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_USERNAME = os.environ.get("otienonick70@gmail.com")
+    MAIL_PASSWORD = os.environ.get("0718849600")
 
-        # simple mde  configurations
+    # simple mde  configurations
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-    
+
     @staticmethod
     def init_app(app):
-        pass
+        pass 
 
 
 
 class ProdConfig(Config):
-       
-
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
     pass
-
-
 
 class TestConfig(Config):
-    
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch_test'
-    pass
+
 
 
 class DevConfig(Config):
     
-  
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/pitch_flask'
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
     DEBUG = True
 
 
@@ -60,10 +62,7 @@ config_options = {
     'development':DevConfig,
     'production':ProdConfig,
     'test':TestConfig
-
 }    
-
-
 
 
 
