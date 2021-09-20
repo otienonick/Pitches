f714d3a779b0d1e5e379595d54be529d49c7312c
otienonick <nick.odongi@student.moringaschool.com>
2021-09-20 20:29:49 +0300

    initial commit

--- migrations/versions/ea330650cd28_.py
+++ migrations/versions/ea330650cd28_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 2c56d0bb4bb9
+Revision ID: ea330650cd28
 Revises: 
-Create Date: 2021-09-20 20:08:00.997453
+Create Date: 2021-09-20 20:26:38.180546
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '2c56d0bb4bb9'
+revision = 'ea330650cd28'
 down_revision = None
 branch_labels = None
 depends_on = None
